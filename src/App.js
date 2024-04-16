2f7e2fe9b394256c8d1d0d5b9eb1b46d79ed06be
James <james440763@hotmail.com>
2024-04-16 22:02:19 +0100

    Lifting up state in preparation for the calculations & displaying output

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
+import React, { useState } from 'react';
 import './App.css';
+import { validWords } from './utils/wordle_words.js';
 import Header from './components/Header.js';
 import WordleInput from './components/WordleInput.js';
 import WordleOutput from './components/WordleOutput.js';
 
 function App() {
+  const [correctLetters, setCorrectLetters] = useState(Array(5).fill(''));
+  const [lettersInWord, setLettersInWord] = useState('');
+  const [lettersNotInWord, setLettersNotInWord] = useState('');
+
+  const handleInputChange = (updatedState) => {
+    setCorrectLetters(updatedState.correctLetters || correctLetters);
+    setLettersInWord(updatedState.lettersInWord || lettersInWord);
+    setLettersNotInWord(updatedState.lettersNotInWord || lettersNotInWord);
+  }
+
+  // Do Worlde code stuff here
+  console.log('Correct Letters: ' + JSON.stringify(correctLetters));
+  console.log('Letters in Word: ' + lettersInWord);
+  console.log('Letters not in Word: ' + lettersNotInWord);
+  console.log(JSON.stringify(validWords));
+
   return (
     <div className="bg-gray-100 h-screen">
       <div className="container mx-auto py-8 px-10 max-w-screen-lg">
         <Header />
         <div className="grid grid-cols-1 md:grid-cols-2 mt-8">
           <div className="w-full">
-            <WordleInput />
+            <WordleInput
+              correctLetters={correctLetters}
+              lettersInWord={lettersInWord}
+              lettersNotInWord={lettersNotInWord}
+              onInputChange={handleInputChange} />
           </div>
           <div className="w-full mt-4 md:mt-0">
             <WordleOutput />
--- src/components/WordleInput.js
+++ src/components/WordleInput.js
@@ -1,42 +1,74 @@
-import React, { useState, useRef } from 'react';
+import React, { useRef } from 'react';
 
-function WordleInput() {
-    const [letters, setLetters] = useState(Array(5).fill(''));
-    const inputRefs = useRef(Array(5).fill(null));
+function WordleInput({ correctLetters, lettersInWord, lettersNotInWord, onInputChange }) {
+  const inputRefs = useRef(Array(5).fill(null));
 
-    const handleChange = (index, event) => {
-        const newLetters = [...letters];
-        newLetters[index] = event.target.value;
-        setLetters(newLetters);
+  const handleUserInput = (event, index = null) => {
+    const updatedState = {};
 
-        // Focus on the next or previous input
-        if (event.target.value && index < 4) {
-            inputRefs.current[index + 1].focus();
-          } else if (!event.target.value && index > 0) {
-            inputRefs.current[index - 1].focus();
-          }
-      };
+    // Update state based on input type
+    if (index !== null) {
+      const newCorrectLetters = [...correctLetters];
+      newCorrectLetters[index] = event.target.value;
+      updatedState.correctLetters = newCorrectLetters;
+    } else {
+      const type = event.target.dataset.type;
+      if (type === 'lettersInWord') {
+        updatedState.lettersInWord = event.target.value;
+      } else {
+        updatedState.lettersNotInWord = event.target.value;
+      }
+    }
 
-    return (
-        <div>
-            <h2 className="text-2xl my-4 text-center text-gray-600">Correct Letters</h2>
-            <div className="flex space-x-2 justify-center">
-                {letters.map((letter, index) => (
-                    <input
-                    key={index}
-                    ref={(el) => (inputRefs.current[index] = el)}
-                    type="text"
-                    maxLength={1}
-                    value={letter}
-                    onChange={(event) => handleChange(index, event)}
-                    className={`w-12 h-12 border border-gray-300 px-3 text-center text-xl font-bold uppercase focus:outline-none ${
-                        letter ? 'wordle-green' : ''
-                    }`}
-                    />
-                ))}
-            </div>
-        </div>
-    );
-  }
-  
-  export default WordleInput;
+    onInputChange(updatedState);
+
+    // Handle focusing on next/previous input (for correctLetters only)
+    if (event.target.value && index !== null && index < 4) {
+      inputRefs.current[index + 1].focus();
+    } else if (!event.target.value && index !== null && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
+  return (
+    <div>
+      <h2 className="text-2xl my-4 text-center text-gray-600">Correct Letters</h2>
+      <div className="flex space-x-2 justify-center">
+        {correctLetters.map((correctLetter, index) => (
+          <input
+            key={index}
+            ref={(el) => (inputRefs.current[index] = el)}
+            type="text"
+            maxLength={1}
+            value={correctLetter}
+            onChange={(event) => handleUserInput(event, index)}
+            data-type="correctLetter"
+            className={`w-12 h-12 border border-gray-300 px-3 text-center text-xl font-bold uppercase focus:outline-none ${
+              correctLetter ? 'wordle-green' : ''
+            }`}
+          />
+        ))}
+      </div>
+      <h2 className="text-2xl my-4 text-center text-gray-600">Letters in Word</h2>
+      <div className="flex justify-center">
+        <input
+          value={lettersInWord}
+          onChange={(event) => handleUserInput(event)}
+          data-type="lettersInWord"
+          className="h-12 border border-gray-300 px-3 text-xl font-bold uppercase focus:outline-none wordle-yellow wordle-input"
+        />
+      </div>
+      <h2 className="text-2xl my-4 text-center text-gray-600">Letters not in Word</h2>
+      <div className="flex justify-center">
+        <input
+          value={lettersNotInWord}
+          onChange={(event) => handleUserInput(event)}
+          data-type="lettersNotInWord"
+          className="h-12 border border-gray-300 px-3 text-xl font-bold uppercase focus:outline-none wordle-gray wordle-input"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default WordleInput;
